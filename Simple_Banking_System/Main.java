0816cc7093671e124a0b66f48599583aaa1ff913
Gattik <gattiktyagi7@gmail.com>
2025-12-25 14:12:23 +0530

    Implement main functionality for Simple Banking System

---
+++ Simple_Banking_System/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main{
+    
+    static void showBalance(int accountNumber){
+    }
+    // static int withdraw(int accountNumber, int amount){
+    // }
+    // static int deposit(int accountNumber,int amount){
+    // }
+    static void exit(){
+        System.out.println("Thank You For Visiting!");
+    }
+    
+    public static void main(String[] args){
+        int choice=0;
+        Scanner scan = new Scanner(System.in);
+        
+        while(true){
+            System.out.print("Welcome to Bank!\nWhats your Account Number? ");
+            int accountNumber = scan.nextInt();
+            scan.nextLine();
+            System.out.println("Your Account Number is "+accountNumber);
+            System.out.print("Yes/No - ");
+            String choiceString = scan.nextLine();
+            if(choiceString.equalsIgnoreCase("yes")){break;}
+        }
+        
+        while(choice!=4){
+            System.out.println("1.Check Balance");
+            System.out.println("2.Withdraw");
+            System.out.println("3.Deposit");
+            System.out.println("4.Exit");
+            choice = 4;
+        }
+
+        scan.close();
+    }
+}
